004568a29087a0f00e2776945e849de79c74fffb
Petrov Kirill <ottokirik@gmail.com>
2021-05-04 19:29:02 +0300

    Реализовано: загрузка и отображение статей на главной странице

--- src/pages/globalFeed/index.js
+++ src/pages/globalFeed/index.js
@@ -1,3 +1,33 @@
+import { useEffect } from 'react';
+import { Feed } from '../../components/feed';
+import { useFetch } from '../../hooks/useFetch';
+
 export const GlobalFeed = () => {
-    return <div>Global feed</div>
-}
+  const apiUrl = '/articles?limit=10&offset=0';
+  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Medium Clone</h1>
+          <p>A place to share knowledge</p>
+        </div>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {isLoading && <div>Loading...</div>}
+            {error && <div>Some error happened.</div>}
+            {!isLoading && response && <Feed articles={response.articles} />}
+          </div>
+          <div className="col-md-3">Popular tags</div>
+        </div>
+      </div>
+    </div>
+  );
+};
